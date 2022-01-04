5177ec69d7a4c5db4301b4eacf320c0c93affb64
effervescent000 <kislio01@gmail.com>
2022-01-04 14:22:46 -0500

    messing with stuff

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express');
 const port = process.env.PORT || 8080;
 const app = express();
 
-app.use(express.static(__dirname + '/dist/'));
+app.use(express.static(__dirname + '/public/'));
 app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(__dirname + '/public/index.html');
 })
 app.listen(port);
 
