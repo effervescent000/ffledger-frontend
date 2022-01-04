04ceac53828cdf7b9a78acfc1de51d67b9eb8521
effervescent000 <kislio01@gmail.com>
2022-01-04 14:32:26 -0500

    i think i got server.js functional

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
+// const express = require('express');
+// const port = process.env.PORT || 8080;
+// const app = express();
+
+// app.use(express.static(__dirname + '/public/'));
+// app.get(/.*/, function (req, res) {
+//   res.sendFile(__dirname + '/public/index.html');
+// })
+// app.listen(port);
+
+// console.log("server started");
+
 const express = require('express');
-const port = process.env.PORT || 8080;
+const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname + '/public/'));
-app.get(/.*/, function (req, res) {
-  res.sendFile(__dirname + '/public/index.html');
-})
-app.listen(port);
+app.use(express.static(path.join(__dirname, 'build')));
 
-console.log("server started");
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(9000);
