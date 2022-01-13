3362afef3a581b63b92543b8b3d6129d1c49592d
effervescent000 <kislio01@gmail.com>
2022-01-13 15:45:13 -0500

    got logout working

--- src/components/auth/logged-in-header.js
+++ src/components/auth/logged-in-header.js
@@ -1,19 +1,43 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
 
 import { UserContext } from "../user-context";
 
 const LoggedInHeader = () => {
     const userContext = useContext(UserContext);
 
-    const handleClick = event => {
+    const handleClick = (event) => {
         event.preventDefault();
         if (userContext.loggedIn) {
-            userContext.toggleLogIn()
-            userContext.setProfile({})
-            localStorage.clear()
+            logoutUser();
         }
-    }
+    };
+
+    const logoutUser = async () => {
+        await axios
+            .post(
+                `${process.env.REACT_APP_DOMAIN}/auth/logout`,
+                {},
+                {
+                    withCredentials: true,
+                    headers: { "X-CSRF-TOKEN": Cookies.get("csrf_access_token") },
+                }
+            )
+            .then((response) => {
+                userContext.setUser({});
+                userContext.setProfile({});
+                userContext.toggleLogIn();
+            })
+            .catch((error) => {
+                if (error.response) {
+                    console.log("error response", error.response);
+                } else {
+                    console.log("some other error logging out", error.response);
+                }
+            });
+    };
 
     return (
         <div id="logged-in">
@@ -26,6 +50,6 @@ const LoggedInHeader = () => {
             </div>
         </div>
     );
-}
+};
 
 export default LoggedInHeader;
