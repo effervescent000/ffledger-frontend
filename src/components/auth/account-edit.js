cded067905c2a334958580e0746ed2f55ee085da
effervescent000 <kislio01@gmail.com>
2022-01-15 12:27:00 -0500

    added account deletion

--- src/components/auth/account-edit.js
+++ src/components/auth/account-edit.js
@@ -1,12 +1,15 @@
 import React, { useState, useContext } from "react";
 import { UserContext } from "../user-context";
 import axios from "axios";
+import { useHistory } from "react-router-dom";
 import Cookies from "js-cookie";
 
 const AccountEditPage = () => {
     const { user, setUser } = useContext(UserContext);
     const [usernameInput, setUsernameInput] = useState(user.username);
     const [emailInput, setEmailInput] = useState("");
+    const [confirmDeleteStyle, setConfirmDeleteStyle] = useState("none");
+    const history = useHistory();
 
     const handleChange = (event) => {
         if (event.target.name === "username-input") {
@@ -18,35 +21,67 @@ const AccountEditPage = () => {
 
     const handleClick = (event) => {
         event.preventDefault();
-        axios
-            .put(
-                `${process.env.REACT_APP_DOMAIN}/auth/update`,
-                {
-                    id: user.id,
-                    username: usernameInput,
-                    email: emailInput,
-                },
-                {
+        if (event.target.name === "save-btn") {
+            axios
+                .put(
+                    `${process.env.REACT_APP_DOMAIN}/auth/update`,
+                    {
+                        id: user.id,
+                        username: usernameInput,
+                        email: emailInput,
+                    },
+                    {
+                        withCredentials: true,
+                        headers: { "X-CSRF-TOKEN": Cookies.get("csrf_access_token") },
+                    }
+                )
+                .then((response) => {
+                    setUser(response.data);
+                })
+                .catch((error) => console.log(error.response));
+        } else if (event.target.naem === "delete-btn") {
+            setConfirmDeleteStyle("block");
+        } else if (event.target.name === "confirm-delete-btn") {
+            axios
+                .delete(`${process.env.REACT_APP_DOMAIN}`, {
                     withCredentials: true,
                     headers: { "X-CSRF-TOKEN": Cookies.get("csrf_access_token") },
-                }
-            )
-            .then((response) => {
-                setUser(response.data);
-            })
-            .catch((error) => console.log(error.response));
+                })
+                .then((response) => {
+                    history.push("/");
+                })
+                .catch((error) => console.log(error.response));
+        }
     };
 
     return (
         <div id="account-edit-wrapper">
-            <input
-                type="text"
-                name="username-input"
-                value={usernameInput}
-                onChange={handleChange}
-            />
-            <input type="email" name="email-input" value={emailInput} onChange={handleChange} />
-            <button onClick={handleClick}>Save</button>
+            <div className="input-wrapper">
+                <input
+                    type="text"
+                    name="username-input"
+                    value={usernameInput}
+                    onChange={handleChange}
+                />
+                <input type="email" name="email-input" value={emailInput} onChange={handleChange} />
+                <button name="save-btn" onClick={handleClick}>
+                    Save
+                </button>
+            </div>
+            <div className="delete-wrapper">
+                <div className="delete-btn">
+                    Delete your account?{" "}
+                    <span className="warning">WARNING: THIS CANNOT BE UNDONE!</span>
+                    <button name="delete-btn" onClick={handleClick}>
+                        Delete account
+                    </button>
+                </div>
+                <div className="secret-delete-btn" style={{ display: confirmDeleteStyle }}>
+                    <button name="confirm-delete-btn" onClick={handleClick}>
+                        Confirm account deletion
+                    </button>
+                </div>
+            </div>
         </div>
     );
 };
